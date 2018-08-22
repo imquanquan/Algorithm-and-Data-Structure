ed95138d0a59a2cdeb29a5447279e2d387aef9c7
imquanquan <imquanquan99@gmail.com>
2018-08-22 18:05:50 +0800

    [fix]Adjust directory structure

--- source/go/src/offer/Problem002.go
+++
@@ -1,7 +0,0 @@
-package offer
-
-import "strings"
-
-func ReplaceBlank(s string) string {
-	return strings.Replace(s, " ", "20%", -1)
-}
--- source/go/src/offer/Problem006.go
+++
@@ -1,9 +0,0 @@
-package offer
-
-func Fibonacci() func() int  {
-	first, second := 0, 1
-	return func() int {
-		first, second = second, first + second
-		return  first
-	}
-}
