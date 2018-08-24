0e1f4730355531137a74266ea686ac3301962ca5
imquanquan <imquanquan99@gmail.com>
2018-08-24 09:44:25 +0800

    [add]add Problem008:fin min in rotate array

---
+++ source/go/Problem008.go
@@ -0,0 +1,35 @@
+package offer
+
+func InOrderFind(a []int) int {
+	result := a[0]
+	for i := 0; i < len(a); i++ {
+		if a[i] < result {
+			result = a[i]
+		}
+	}
+	return result
+}
+
+func FindMinInRotateArray(a []int) int {
+	if len(a) == 0 {
+		return -1
+	}
+	mid, left, right := 0, 0, len(a) - 1
+
+	for a[left] >= a[right] {
+		if right - left == 1 {
+			return a[right]
+		}
+		mid := (left + right) / 2
+		if a[left] == a[mid] && a[mid] == a[right] {
+			return InOrderFind(a)
+		}
+		if a[left] <= a[mid] {
+			left = mid
+		}
+		if a[right] >= a[mid] {
+			right = mid
+		}
+	}
+	return a[mid]
+}
